d4544581c76f6555664b0f083b50ad0be6c5ab6b
McFord <McFord@ukr.net>
2020-09-27 20:23:35 +0300

    Добавлена диархическая система персонажей Добавлена доска обучения Фокус камеры увеличен до 12f
    Багуфикс:
    - Артефакты на уровне
    - Пушка следовала за курсором после выстрела

---
+++ Assets/Scripts/Castling.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castling : MonoBehaviour
+{
+    public GameObject celestiaPlayer;
+    public GameObject lunaPlayer;
+    public Renderer celestiaSprite;
+    public Renderer lunaSprite;
+    Animator celestiAnimatorController;
+    Animator lunAnimatorController;
+    Player player;
+
+    public GameObject celestia;
+    public GameObject luna;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+
+        celestiAnimatorController = celestiaPlayer.GetComponent<Animator>();
+        lunAnimatorController = lunaPlayer.GetComponent<Animator>();
+
+        celestiaPlayer.SetActive(false);
+        player.sprite = lunaSprite;
+        player.animatorController = lunAnimatorController;
+    }
+
+    public void ChangePrincess()
+    {
+        if (celestiaPlayer.activeSelf)
+        {
+            player.animatorController = lunAnimatorController;
+            player.sprite = lunaSprite;
+            celestiaPlayer.SetActive(false);
+            lunaPlayer.SetActive(true);
+
+            luna.SetActive(false);
+            celestia.SetActive(true);
+        }
+        else
+        {
+            player.animatorController = celestiAnimatorController;
+            player.sprite = celestiaSprite;
+            lunaPlayer.SetActive(false);
+            celestiaPlayer.SetActive(true);
+
+            celestia.SetActive(false);
+            luna.SetActive(true);
+        }
+
+        player.Reset();
+    }
+}
