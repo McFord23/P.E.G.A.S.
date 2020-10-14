4ecc3dad7970ad5ce9c87cce8bf41ead1e1da7c1
canis <52971880+remarka-net@users.noreply.github.com>
2020-10-14 19:05:42 +0300

    Revert "123 test changes"
    This reverts commit 206e27de09ec431e20717a436cf47723f7c73751.

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -7,6 +7,12 @@ using UnityEngine.UI;
 public class AudioController : MonoBehaviour
 {
     public AudioMixerGroup mixer;
+<<<<<<< HEAD
+
+    public void EnableSound()
+    {
+        Save.Sound = !Save.Sound;
+=======
     public Slider volume;
     public Toggle soundToggle;
     public Toggle musicToggle;
@@ -21,20 +27,33 @@ public class AudioController : MonoBehaviour
     public void EnableSound(bool value)
     {
         Save.Sound = value;
+>>>>>>> kpereb
         if (Save.Sound) mixer.audioMixer.SetFloat("SoundVolume", 0);
         else mixer.audioMixer.SetFloat("SoundVolume", -80);
     }
 
-
+<<<<<<< HEAD
+    public void EnableMusic()
+    {
+        Save.Music = !Save.Music;
+=======
     public void EnableMusic(bool value)
     {
         Save.Music = value;
+>>>>>>> kpereb
         if (Save.Music) mixer.audioMixer.SetFloat("MusicVolume", 0);
         else mixer.audioMixer.SetFloat("MusicVolume", -80);
     }
 
     public void ChangeVolume(Slider volume)
     {
+<<<<<<< HEAD
+        Save.Volume = volume.value;
+        mixer.audioMixer.SetFloat("MasterVolume", Mathf.Lerp(-80, 0, Save.Volume));
+=======
+        //Save.Volume = volume.value;
+        //mixer.audioMixer.SetFloat("MasterVolume", Mathf.Lerp(-80, 0, Save.Volume));
         AudioListener.volume = volume.value;
+>>>>>>> kpereb
     }
 }
