d082ad087f7bc3eb7c9bf33702974276c9d64fa5
McFord <McFord@ukr.net>
2020-10-23 16:15:08 +0300

    Добавлены музыкальные темы, что меняются при рокировке принцесс > Добавлена возможность настраивать громкость игры > Добавлена возможность глушить музыку > Добавлена возможность глушить звуки
    Баги:
    + Нет синхронизации заглушек музыки и звуков

---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Save
+{
+    static string princessName = "Celestia";
+    static bool soundEnable = true;
+    static bool musicEnable = true;
+    static float masterVolume = 1;
+
+    public static string Princess
+    {
+        get
+        {
+            return princessName;
+        }
+        set
+        {
+            princessName = value;
+        }
+    }
+
+    public static bool Sound
+    {
+        get
+        {
+            return soundEnable;
+        }
+        set
+        {
+            soundEnable = value;
+        }
+    }
+
+    public static bool Music
+    {
+        get
+        {
+            return musicEnable;
+        }
+        set
+        {
+            musicEnable = value;
+        }
+    }
+
+    public static float Volume
+    {
+        get
+        {
+            return masterVolume;
+        }
+        set
+        {
+            masterVolume = value;
+        }
+    }
+}
