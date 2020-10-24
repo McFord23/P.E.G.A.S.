7ef82e58827d96161d62bc46e5e8657bde8e1759
McFord <McFord@ukr.net>
2020-10-24 03:22:01 +0300

    Добавлена механика шмаляния огненными шарами

---
+++ Assets/Scripts/FireballController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour
+{
+    public GameObject fireball;
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Instantiate(fireball,transform.position,transform.rotation);
+        }
+    }
+}
