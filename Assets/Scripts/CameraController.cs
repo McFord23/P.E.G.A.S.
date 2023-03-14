da90fd706cb26f1dc621abdedcc47bf9fdd7a985
McFord <McFord@ukr.net>
2023-03-14 14:01:17 +0200

    PEGAS-2 | Добавлены отладочные сани > Режим камеры сменён на перспективу > Добавлено освещение с картой отражений

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,10 +9,11 @@ public class CameraController : MonoBehaviour
     Vector3 interfaceOffset;
     Vector3 mapOffset;
 
-    public static float maxSize = 25f;
-    public static float flySize = 13f;
-    public static float minSize = 5f;
+    public static float maxSize = 42f; // ort = 25
+    public static float flySize = 22f; // ort = 13
+    public static float minSize = 10f; // ort = 5
     float size;
+    float zoomSpeed = 0.5f;
 
     void Start()
     {
@@ -30,6 +31,7 @@ public class CameraController : MonoBehaviour
             case "fly":
                 if (playersController.GetPlayerDirection() > 0) playerOffset = new Vector3(10, 0, 0);
                 else if (playersController.GetPlayerDirection() < 0) playerOffset = new Vector3(-10, 0, 0);
+
                 if (Save.TogetherMode)
                 {
                     if (Save.Player1.live && Save.Player2.live)
@@ -51,7 +53,7 @@ public class CameraController : MonoBehaviour
                 {
 
                     transform.position = Vector3.Lerp(transform.position, playersController.GetPlayerPosition() + playerOffset + mapOffset, Time.deltaTime * 5f);
-                    if (size < flySize) size += 0.1f;
+                    if (size < flySize) size += zoomSpeed;
 
                 }
                 break;
@@ -59,16 +61,16 @@ public class CameraController : MonoBehaviour
                 transform.position = Vector3.Lerp(transform.position, playersController.GetPlayerPosition() + interfaceOffset, Time.deltaTime * 5f);
                 if (!Save.TogetherMode)
                 {
-                    if (size > minSize) size -= 0.1f;
+                    if (size > minSize) size -= zoomSpeed;
                 }
                 break;
             case "heart":
                 transform.position = Vector3.Lerp(transform.position, playersController.heart.transform.position + interfaceOffset, Time.deltaTime * 5f);
-                if (size > minSize) size -= 0.1f;
+                if (size > minSize) size -= zoomSpeed;
                 break;
         }
 
-        Camera.main.orthographicSize = size;
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -size);
     }
 
     public void FocusOnFly()
